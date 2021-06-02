93e9de5952dccf5df50055fef6f09b4196aed95f
Nick Dunn <nick.dunn@flosports.tv>
2021-06-02 09:33:19 -0500

    feat: introduce card-list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
 
 const EXTERNAL_USERS_API = 'https://jsonplaceholder.typicode.com/users'
 
@@ -20,13 +21,15 @@ class App extends Component {
 
   render() {
     return (
-        <div className="App">
+      <div className="App">
+        <CardList>
           {
             this.state.monsters.map(monster => {
               return <h1 key={ monster.id }>{ monster.name }</h1>
             })
           }
-        </div>
+        </CardList>
+      </div>
     )
   }
 }
