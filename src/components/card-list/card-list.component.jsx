8b7c54ad7057eeda453b99918c220587a2560876
Nick Dunn <nick.dunn@flosports.tv>
2021-06-09 09:17:12 -0500

    fix: move mapping to CardList

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './card-list.styles.css'
 
-export const CardList = (props) => {
-  return (<div className='card-list'>{ props.children }</div>)
-}
+export const CardList = (props) => (
+  <div className='card-list'>
+    {
+      props.monsters.map(monster => {
+        return <h1 key={ monster.id }>{ monster.name }</h1>
+      })
+    }
+  </div>
+)
