e8047b327b23348e36f4f6cc44497705b1065856
Nick Dunn <nick.dunn@flosports.tv>
2021-06-15 09:47:33 -0500

    feat: introduce SearchBox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { Component } from 'react'
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 const EXTERNAL_USERS_API = 'https://jsonplaceholder.typicode.com/users'
 
@@ -28,10 +29,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monsters'
-          onChange={e => this.setState({ searchField: e.target.value })}/>
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={ filteredMonsters }/>
       </div>
     )
